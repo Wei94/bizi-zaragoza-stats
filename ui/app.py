db0d5e9dc3e6d2e1eb6d161615eb3ace7054b0fb
Jorge Elias <jorge_fdf@hotmail.com>
2026-08-25 22:18:55 +0200

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
+import time
 
 # Configuración de la página
 st.set_page_config(page_title="Bizi Zaragoza Live & Analytics", layout="wide", page_icon="\U0001F6B2")
